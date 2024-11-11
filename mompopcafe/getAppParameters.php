0518528f489789f489222baf89ae222ce6ee50c7
Hitesh-gp <115928787+Hitesh-gp@users.noreply.github.com>
2024-11-11 18:22:38 +0530

    Update getAppParameters.php

--- mompopcafe/getAppParameters.php
+++ mompopcafe/getAppParameters.php
@@ -9,14 +9,6 @@
   $db_user = "hitesh";
   $db_password = "Msois@123";
 
-  $conn = new mysqli($db_url, $db_user, $db_password, $db_name);
-
-// Check if the connection was successful
-  if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-  } else {
-      echo "Successfully connected to the database.";
-  }
   // Display the server metadata information if the showServerInfo parameter is true.
   //include('serverInfo.php');
 ?>
